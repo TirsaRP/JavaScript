4a766bf209b0f5002e77dbe535a2c1d205b49bdc
Tirsa <tr.galvan@gmail.com>
2018-04-01 09:33:40 +0200

    week-1 assignment

---
+++ week-1.js
@@ -0,0 +1,79 @@
+//console exercise
+
+console.log("Hello World!"); //english
+console.log("Hola Mundo!"); //Spanish
+console.log("Hej Världen!"); //Swedish
+
+var  x 
+console.log("the value of my variable x will be:undefined"); 
+console.log("value of x:" +x);
+var x = 2
+console.log("the value of x will be:2");
+console.log("value of x:" +x);
+
+var y ="hello"
+console.log("the value of my string will be:hello");
+var y = "world"
+console.log("the value of my string will be:world");
+console.log(y);
+
+var z = 7.25
+console.log(z);
+var a = Math.round(z);
+console.log(a);
+z === a
+var b =Math.max(a,z);
+console.log(b);
+
+//arrays
+var plurality =[];
+console.log("it is an empty array");
+console.log(plurality);
+var favoriteAnimals =["wolf","dolphin", "koala", "panda"]
+console.log(favoriteAnimals); 
+favoriteAnimals.push("baby pig");
+console.log(favoriteAnimals); 
+
+//strings
+let myString = "this is a test"
+rosaString="hello there"
+console.log(rosaString);
+myString.length;
+console.log(rosaString.length);
+
+var a= 1
+var b= "cat"
+var c= true
+var d= 1.5
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+//confused with 8.3-8.6
+if (typeof a !== typeof b) {
+    console.log('NOT SAME TYPE');
+}
+if (typeof a !== typeof d) {
+    console.log('NOT SAME TYPE');
+}
+
+
+x=7 
+x=x % 3 
+x=1
+console.log("% is an arithmetic operator");
+console.log("% stands for remainder");
+console.log("ex: 9%2=1");
+console.log(13%3);
+
+//10.
+var myArray=["car", "box", 1, 4];
+isFinite(6/0) === isFinite(10/0);
+console.log(isFinite(6/0) === isFinite(10/0));
+
+
